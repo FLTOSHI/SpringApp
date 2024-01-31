5674f091a207ef59242aa521014dcbca2505216c
FLTOSHI <fltoshimyname@gmail.com>
2024-02-01 01:08:59 +0300

    Ночные посиделки в попытках разобраться с ошибочным заполнением application.properties из-за приступа овоща.

--- src/main/java/com/example/demo/entity/criminalEntity.java
+++ src/main/java/com/example/demo/entity/criminalEntity.java
@@ -1,10 +1,17 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Table(name = "criminal")
 public class criminalEntity {
+    @Column(name = "criminal_id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
